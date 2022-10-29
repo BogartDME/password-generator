b67b984ac9d8a19319f15edd9bbd8500510e3992
BogartDME <edwin.pietrowski@gmail.com>
2022-10-29 18:50:32 -0400

    more JS work

--- script.js
+++ script.js
@@ -11,55 +11,76 @@ const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "
 const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 const specialCharacters =["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "/", ">", "<", "."]
-let possibleCharacters =[]
+var possibleCharacters =[]
 
-function askUser (userName){
+function greetUser (userName){
   //
-  const passLength= parseInt(window.prompt("Hi " + userName + "Choose how many characters your password should be"))
+  const passLength= parseInt(window.prompt("Hello, " + userName + ". Choose how many characters your password should be"))
   if (passLength<8 ||passLength>128){
     alert("Please choose a valid number between 8 and 128")
     return; // if true return will return to the function and start over. If false it will continue to the next if
   } 
   
   if (isNaN(passLength)){
-    alert("Please choose a valid number")
-    return;
+    alert("Please choose a valid number, human.")
+    return; // if true return will return to the function and start over. If false it will continue to the booleans for characters
   }
 
+  //create confirm for lowercase (Y, N)
   const responseLower = confirm("Would you like your password to contain lowercase letters?")
   console.log (responseLower)
   if (responseLower) {
     possibleCharacters=possibleCharacters.concat(lowercase)
   }
   
-  //create prompt for uppercase (Y, N)
+  //create confirm for uppercase (Y, N)
   const responseUpper = confirm("Would you like your password to contain uppercase letters?")
   console.log(responseUpper)
   if (responseUpper) {
     possibleCharacters=possibleCharacters.concat(uppercase)
   }
-  //create prompt for numbers (Y, N)
+
+  //create confirm for numbers (Y, N)
   const responseNumbers = confirm("Would you like your password to contain numbers?")
   console.log(responseNumbers)
   if (responseNumbers) {
     possibleCharacters=possibleCharacters.concat(numbers)
   }
   
-  //create prompt for special characters (Y, N)
-  // const responseSpecial = confirm("Would you like your password to contain special characters?")
+  //create confirm for special characters (Y, N)
+  const responseSpecial = confirm("Would you like your password to contain special characters?")
   console.log (possibleCharacters);
   if(possibleCharacters.length===0){
     alert("Please choose atleast one character type")
-    return;
+  
+    
   }
-  return {possibleCharacters, passLength}
+ 
+  let password= " ";
+  
+  for (var i = 0; i < passLength; i++)
+  
+  {var endResult = Math.floor(Math.random() * possibleCharacters.length);}
+  // return {possibleCharacters, passLength}
+  
+  password += possibleCharacters[endResult]
+  console.log (endResult)
+  console.log (password)
+  
+  
+  
 }
 
-//create prompt for lowercase (Y, N)
+// return password;
 
-//validate input and ensure one character type selected
+//for loop to create password with user input 
 
-//function to create output with user input
+
+
+
+
+
+
 
 //generate password
 
@@ -67,11 +88,11 @@ function askUser (userName){
 //display password
 
 function generatePassword(){ 
-  var name = "Edwin";  
-  var options = askUser(name)
-  console.log (options)
-return "fake password"
-}
+  var name = "human ";  
+  var options = greetUser(name)
+  // console.log (options)
+// return "fake password"
+
 
 // Write password to the #password input
 function writePassword() {
